fe5e09800209ba90bc4376ca0fd6fab5f4dfb78c
nothingADSR123 <nothingADSR123@gmail.com>
2025-10-01 09:42:19 +0530

    Starting with Graphs

---
+++ TopSWE/200.NumberofIslands.py
@@ -0,0 +1,71 @@
+# 200. Number of Islands
+# Medum
+# Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+# An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+# Example 1:
+# Input: grid = [
+#   ["1","1","1","1","0"],
+#   ["1","1","0","1","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","0","0","0"]
+# ]
+# Output: 1
+# Example 2:
+# Input: grid = [
+#   ["1","1","0","0","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","1","0","0"],
+#   ["0","0","0","1","1"]
+# ]
+# Output: 3
+
+from typing import Optional,List,deque
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        # DFS recursively solution 
+        # if not grid :
+        #     return 0
+        # rows,cols=len(grid),len(grid[0])
+
+        # def dfs(r,c):
+        #     if r < 0 or r>=rows or c<0 or c>=cols or grid[r][c]=="0":
+        #         return 
+        #     grid[r][c]="0" #Mark visited
+        #     dfs(r+1,c)
+        #     dfs(r-1,c)
+        #     dfs(r,c-1)
+        #     dfs(r,c+1)
+        
+        # islands=0
+        # for r in range(rows):
+        #     for c in range(cols):
+        #         if grid[r][c]=="1":
+        #             islands+=1
+        #             dfs(r,c)
+        # return islands
+
+        # BFS solution 
+        if not grid :
+            return 0
+        rows,cols=len(grid),len(grid[0])
+
+        def bfs(r,c):
+            q=deque([(r,c)])
+            while q:
+                x,y=q.popleft()
+                for dx,dy in [(1,0),(-1,0),(0,1),(0,-1)]:
+                    nx,ny=x+dx,y+dy
+                    if 0<=nx<rows and 0<=ny<cols and grid[nx][ny]=="1":
+                        grid[nx][ny]="0"
+                        q.append((nx,ny))
+
+
+
+        islands=0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=="1":
+                    islands+=1
+                    bfs(r,c)
+        return islands
