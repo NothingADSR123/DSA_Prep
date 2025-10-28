e74ce9b2f064f95577bdc411852e11a5ff0819ea
nothingADSR123 <nothingADSR123@gmail.com>
2025-10-28 20:54:22 +0530

    Did Backtracking khud se

---
+++ TopSWE/77.Combinations.py
@@ -0,0 +1,33 @@
+# 77. Combinations
+# Medium
+# Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+# You may return the answer in any order.
+# Example 1:
+# Input: n = 4, k = 2
+# Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+# Explanation: There are 4 choose 2 = 6 total combinations.
+# Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination.
+# Example 2:
+# Input: n = 1, k = 1
+# Output: [[1]]
+# Explanation: There is 1 choose 1 = 1 total combination.
+
+from typing import List
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        # SOLVED KHUD LOGIC BANAKE OYEEEEEEEEEEEEEEEEEEEEEEE
+        # YEE EHEHEHE WO HOOOOO
+        initial=[]
+        for i in range (n):
+            initial.append(i+1)
+        res=[]
+        def backtrack (index,numbers):
+            if len(numbers)==k:
+                res.append(numbers[:])
+                return
+            for i in range(index,n):
+                numbers.append(initial[i])
+                backtrack(i+1,numbers)
+                numbers.pop()
+        backtrack(0,[])
+        return res
