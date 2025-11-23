01f85f166dc7b9974aad7251ef625dcaf9dfe8a3
nothingADSR123 <nothingADSR123@gmail.com>
2025-11-23 13:52:42 +0530

    Hard Heap Nice question

---
+++ TopSWE/295.FindMedianfromDataStream.py
@@ -0,0 +1,58 @@
+# 295. Find Median from Data Stream
+# Hardk icon
+# Companie
+# The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+# For example, for arr = [2,3,4], the median is 3.
+# For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+# Implement the MedianFinder class
+# MedianFinder() initializes the MedianFinder object.
+# void addNum(int num) adds the integer num from the data stream to the data structure.
+# double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+# Example 1:
+# Input
+# ["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+# [[], [1], [2], [], [3], []]
+# Output
+# [null, null, null, 1.5, null, 2.0]
+# Explanation
+# MedianFinder medianFinder = new MedianFinder();
+# medianFinder.addNum(1);    // arr = [1]
+# medianFinder.addNum(2);    // arr = [1, 2]
+# medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+# medianFinder.addNum(3);    // arr[1, 2, 3]
+# medianFinder.findMedian(); // return 2.0
+from typing import List
+import heapq
+class MedianFinder:
+# An efficient way is to use heap and other ways like sorting and all is not optimal thats why 
+# and this is a good question refer yt to understand 
+# Nice trick
+    def __init__(self):
+        self.small=[] #This is a max heao but implemented with a negative sign
+        self.large=[] #This is a min heap
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small,-num)
+
+        if self.small and self.large and (-self.small[0]>self.large[0]):
+            val=heapq.heappop(self.small)
+            heapq.heappush(self.large,-val)
+        if len(self.small)>len(self.large)+1:
+            val = -heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.large)>len(self.small)+1:
+            val=-heapq.heappop(self.large)
+            heapq.heappush(self.small,val)
+    def findMedian(self) -> float:
+        if len(self.small)>len(self.large):
+            return -self.small[0]
+        if len(self.large)>len(self.small):
+            return self.large[0]
+        else:
+            return (-self.small[0]+self.large[0])/2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
